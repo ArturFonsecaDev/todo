721cf55fe2664cb61bb50572db6a515ed81ef9c8
trdntb <130498186+trdntb@users.noreply.github.com>
2024-09-15 22:33:04 -0300

    .

--- front/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.kanban_list, name='kanban-list'),
-    path('<str:kanban_name>/', views.kanban_detail, name='kanban-detail'),
-]
--- todoapi/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class TodoapiConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "todoapi"
-    
-    def ready(self):
-        import todoapi.signals
